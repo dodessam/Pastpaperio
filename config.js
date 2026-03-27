a039de1a94aed0944fea0bfd1229282fbb35da01
dodessam <danoessam.saad@gmail.com>
2026-03-27 23:53:49 +0200

    Add API handler for Firebase configuration

---
+++ config.js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+  // This runs securely on Vercel's servers, pulling from your Environment Variables
+  res.status(200).json({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+  });
+}
